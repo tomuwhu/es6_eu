68ea7c1a763ea3ca494848c0773750efc1b0e594
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-10-15 10:38:47 +0200

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,91 +1,47 @@
 <h1 class="text-center mb-5 mt-4">ES6.EU</h1>
 
 <div class="container text-center">
-	<div class="row">
-		<div class="col m-2 x">
-			<a href="https://szte.es6.eu" target="_blank">Egyetemi oktatás</a>
-		</div>
-		<div class="col m-2 x">
-			<a href="https://sob.es6.eu" target="_blank">Szakképzési oldal</a>
-		</div>
-		<div class="col m-2 x">
-			<a href="https://tomuwhu.github.io/sva/" target="_blank">Ságváris oktatás</a>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col m-2 x">
-			<a href="gimn_progr" target="_blank">Programozás szakkör</a>
-		</div>
-		<div class="col m-2 x">
-			<a href="https://gyakg.es6.eu" target="_blank">Régi közoktatási oldal</a>
-		</div>
-		<div class="col m-2 x">
-			<a href="https://www.inf.u-szeged.hu/~tnemeth" target="_blank">Régi egyetemi oldal</a>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col m-2 y">
-			<a class="y" href="https://www.youtube.com/watch?v=A1BhOVW8qZU" target="_blank"
+			<a class="btn btn-outline-secondary btn-sm" href="https://szte.es6.eu" target="_blank">Egyetemi oktatás</a>
+			<a class="btn btn-outline-danger btn-sm" href="https://sob.es6.eu" target="_blank">Szakképzési oldal</a>
+			<a class="btn btn-outline-secondary btn-sm" href="https://tomuwhu.github.io/sva/" target="_blank">Ságváris oktatás</a>
+			<a class="btn btn-outline-primary btn-sm" href="gimn_progr" target="_blank">Programozás szakkör</a>
+			<a class="btn btn-outline-success btn-sm" href="https://gyakg.es6.eu" target="_blank">Közoktatási oldal</a>
+			<a class="btn btn-outline-secondary btn-sm" href="https://www.inf.u-szeged.hu/~tnemeth" target="_blank">Egyetemi oldal</a>
+			<a class="btn btn-outline-secondary btn-sm y" href="https://www.youtube.com/watch?v=A1BhOVW8qZU" target="_blank"
 				>Einstein Tile</a
 			>
-		</div>
-	</div>
 </div>
 
 <div class="container text-center">
-	<div class="row mt-5">
-		<div class="col m-2" />
-		<div class="col m-2">
-			<a href="https://github.com/tomuwhu/es6_eu" target="_blank" class="ghl">
-				<img
-					width="50px"
-					src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
-					alt=""
-				/>
-			</a>
-		</div>
-		<div class="col m-2" />
-	</div>
+	<br>
+	<a href="https://github.com/tomuwhu/es6_eu" target="_blank" class="ghl">
+		<img
+			width="50px"
+			src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+			alt=""
+		/>
+	</a>
 </div>
-
 <style>
-	a {
-		display: inline-block;
+	.btn {
 		white-space: nowrap;
-		border: solid 1px black;
+	}
+	a {
+        margin: 5px;
+    }
+	img {
 		border-radius: 6px;
-		padding: 6px;
-		width: 250px;
-		background-color: rgb(179, 232, 214);
-	}
-	div.y a {
-		background-color: rgb(209, 197, 221);
-	}
-	a:hover {
-		background-color: aquamarine;
-	}
-	a:hover {
-		color: rgb(32, 158, 175);
-	}
-	a:hover {
-		color: rgb(36, 103, 112);
-	}
-	div a {
-		text-decoration: none;
-		color:rgb(90, 6, 6);
-	}
-	a.ghl {
 		background-color: white;
-		border: solid 1px gray;
+		box-shadow: 1px 1px 4px black;
+		border: solid 2px white;
 	}
-	a.ghl:hover {
-		background-color: white;
-		border: solid 1px black;
-		box-shadow: 1px 1px 3px black;
+	img:hover {
+		box-shadow: 1px 1px 6px rgb(30, 15, 15);
+		border: solid 2px black;
 	}
-	a.ghl:active {
-		background-color: white;
-		border: solid 1px black;
-		box-shadow: 1px 1px 3px inset black;
+	img:active {
+		box-shadow: 1px 1px 1px rgb(30, 15, 15);
+		border: solid 2px black;
 	}
 </style>
+
--- src/routes/gimn_progr/+page.svelte
+++ src/routes/gimn_progr/+page.svelte
@@ -5,11 +5,12 @@
     <a class="btn btn-outline-secondary btn-sm" href="https://www.tankonyvkatalogus.hu/pdf/OH-DIG09TA__teljes.pdf">Tankönyv 9.</a>
     <a class="btn btn-outline-secondary btn-sm" href="https://www.tankonyvkatalogus.hu/pdf/OH-DIG10TA__teljes.pdf">Tankönyv 10.</a>
     <a class="btn btn-outline-primary btn-sm" href="https://gyakg.es6.eu/irodalom/html.pdf">HTML-CSS tankönyv</a>
-    <a class="btn btn-primary btn-sm" href="https://gyakg.es6.eu/irodalom/python-a-gepben.epub">Python tankönyv</a>
+    <a class="btn btn-outline-primary btn-sm" href="https://gyakg.es6.eu/irodalom/python-a-gepben.epub">Python tankönyv</a>
     <a class="btn btn-outline-primary btn-sm" href="https://gyakg.es6.eu/irodalom/SQLbook.pdf">SQL tankönyv</a>
-    <a class="btn btn-outline-primary btn-sm" href="http://tehetseg.inf.elte.hu/jegyzetek/SzaboZsanett_AlgoritmizaljunkFlowgorithmmel.pdf">Flowgorithm</a>
+    <a class="btn btn-primary btn-sm" href="http://tehetseg.inf.elte.hu/jegyzetek/SzaboZsanett_AlgoritmizaljunkFlowgorithmmel.pdf">Flowgorithm</a>
     <a class="btn btn-danger btn-sm" href="https://gyakg.es6.eu/pyexamples.html">Python példaprogramok</a>
-    <br>
+    <a class="btn btn-success btn-sm" href="https://nandgame.com/">Nand Game</a>
+    <a class="btn btn-success btn-sm" href="https://gyakg.es6.eu/ass/">Assembler szimulátor</a>
     <a class="btn btn-success btn-sm" href="https://cses.fi/problemset/">CSES Problem Set</a>
     <a class="btn btn-warning btn-sm" href="https://gyakg.es6.eu/efntmego.html">Megoldások</a>
 </div>
