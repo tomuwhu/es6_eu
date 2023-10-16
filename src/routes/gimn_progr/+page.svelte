84178a3ac2c0282e29ba7992b995bb2f7d434123
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-10-16 15:28:42 +0200

    .

--- src/routes/gimn_progr/+page.svelte
+++ src/routes/gimn_progr/+page.svelte
@@ -9,6 +9,7 @@
     <a class="btn btn-outline-primary btn-sm" href="https://gyakg.es6.eu/irodalom/SQLbook.pdf">SQL tankönyv</a>
     <a class="btn btn-primary btn-sm" href="http://tehetseg.inf.elte.hu/jegyzetek/SzaboZsanett_AlgoritmizaljunkFlowgorithmmel.pdf">Flowgorithm</a>
     <a class="btn btn-danger btn-sm" href="https://gyakg.es6.eu/pyexamples.html">Python példaprogramok</a>
+    <a class="btn btn-danger btn-sm" href="https://gyakg.es6.eu/nodeexamples.html">NdeJS példaprogramok</a>
     <a class="btn btn-success btn-sm" href="https://nandgame.com/">Nand Game</a>
     <a class="btn btn-success btn-sm" href="https://gyakg.es6.eu/ass/">Assembler szimulátor</a>
     <a class="btn btn-success btn-sm" href="https://cses.fi/problemset/">CSES Problem Set</a>
