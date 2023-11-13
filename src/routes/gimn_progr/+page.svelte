dfd0dc55141657b4271a0c62468e3fe159f5aced
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-11-13 07:02:14 +0100

    .

--- src/routes/gimn_progr/+page.svelte
+++ src/routes/gimn_progr/+page.svelte
@@ -2,20 +2,21 @@
     <br>
     <h1>Szakkör tananyag</h1>
     <br>
-    <a class="btn btn-outline-secondary btn-sm" href="https://www.tankonyvkatalogus.hu/pdf/OH-DIG09TA__teljes.pdf">Tankönyv 9.</a>
-    <a class="btn btn-outline-secondary btn-sm" href="https://www.tankonyvkatalogus.hu/pdf/OH-DIG10TA__teljes.pdf">Tankönyv 10.</a>
-    <a class="btn btn-outline-primary btn-sm" href="https://gyakg.es6.eu/irodalom/html.pdf">HTML-CSS tankönyv</a>
-    <a class="btn btn-outline-primary btn-sm" href="https://gyakg.es6.eu/irodalom/python-a-gepben.epub">Python tankönyv</a>
-    <a class="btn btn-outline-primary btn-sm" href="https://gyakg.es6.eu/irodalom/SQLbook.pdf">SQL tankönyv</a>
-    <a class="btn btn-outline-primary btn-sm" href="http://tehetseg.inf.elte.hu/jegyzetek/SzaboZsanett_AlgoritmizaljunkFlowgorithmmel.pdf">Flowgorithm</a>
-    <a class="btn btn-outline-danger btn-sm" href="https://gyakg.es6.eu/pyexamples.html">Python példaprogramok</a>
-    <a class="btn btn-outline-danger btn-sm" href="https://gyakg.es6.eu/nodeexamples.html">NodeJS példaprogramok</a>
-    <a class="btn btn-outline-success btn-sm" href="https://nandgame.com/">Nand Game</a>
-    <a class="btn btn-outline-success btn-sm" href="https://gyakg.es6.eu/ass/">Assembler szimulátor</a>
-    <a class="btn btn-outline-success btn-sm" href="https://cses.fi/problemset/">CSES Problem Set</a>
-    <a class="btn btn-warning btn-sm" href="https://gyakg.es6.eu/efntmego.html">Megoldások</a>
-    <a class="btn btn-warning btn-sm" href="https://tomuwhu.github.io/Aknakereso-/">Órai anyag: Aknakereső futtatása</a>
-    <a class="btn btn-warning btn-sm" href="https://github.com/tomuwhu/Aknakereso-/">Órai anyag: Aknakereső forrás</a>
+    <a class="btn btn-outline-secondary btn-sm" target="_blank" href="https://www.tankonyvkatalogus.hu/pdf/OH-DIG09TA__teljes.pdf">Tankönyv 9.</a>
+    <a class="btn btn-outline-secondary btn-sm" target="_blank" href="https://www.tankonyvkatalogus.hu/pdf/OH-DIG10TA__teljes.pdf">Tankönyv 10.</a>
+    <a class="btn btn-warning btn-sm" target="_blank" href="https://www.nkp.hu/tankonyv/digitalis_kultura_11-12_okosgyujtemeny_emelt_szint/lecke_08_005">Okostankönyv: Típusalgoritmusok</a>
+    <a class="btn btn-outline-primary btn-sm" target="_blank" href="https://gyakg.es6.eu/irodalom/html.pdf">HTML-CSS tankönyv</a>
+    <a class="btn btn-outline-primary btn-sm" target="_blank" href="https://gyakg.es6.eu/irodalom/python-a-gepben.epub">Python tankönyv</a>
+    <a class="btn btn-outline-primary btn-sm" target="_blank" href="https://gyakg.es6.eu/irodalom/SQLbook.pdf">SQL tankönyv</a>
+    <a class="btn btn-outline-primary btn-sm" target="_blank" href="http://tehetseg.inf.elte.hu/jegyzetek/SzaboZsanett_AlgoritmizaljunkFlowgorithmmel.pdf">Flowgorithm</a>
+    <a class="btn btn-outline-danger btn-sm" target="_blank" href="https://gyakg.es6.eu/pyexamples.html">Python példaprogramok</a>
+    <a class="btn btn-outline-danger btn-sm" target="_blank" href="https://gyakg.es6.eu/nodeexamples.html">NodeJS példaprogramok</a>
+    <a class="btn btn-outline-success btn-sm" target="_blank" href="https://nandgame.com/">Nand Game</a>
+    <a class="btn btn-outline-success btn-sm" target="_blank" href="https://gyakg.es6.eu/ass/">Assembler szimulátor</a>
+    <a class="btn btn-outline-success btn-sm" target="_blank" href="https://cses.fi/problemset/">CSES Problem Set</a>
+    <a class="btn btn-warning btn-sm" target="_blank" href="https://gyakg.es6.eu/efntmego.html">Megoldások</a>
+    <a class="btn btn-outline-warning btn-sm" target="_blank" href="https://tomuwhu.github.io/Aknakereso-/">Órai anyag: Aknakereső futtatása</a>
+    <a class="btn btn-outline-warning btn-sm" target="_blank" href="https://github.com/tomuwhu/Aknakereso-/">Órai anyag: Aknakereső forrás</a>
 </div>
 <style>
     a {
