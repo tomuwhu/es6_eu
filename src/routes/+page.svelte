60e93862dacdf14015af096741affa260be41a52
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-06-14 11:40:14 +0200

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,10 @@
 <div class="container text-center">
 			<a class="btn btn-outline-secondary btn-sm" href="https://szte.es6.eu" target="_blank">Egyetemi oktatás</a>
 			<a class="btn btn-outline-danger btn-sm" href="https://sob.es6.eu" target="_blank">Szakképzési oldal</a>
+			<a class="btn btn-outline-success btn-sm" href="https://gyakg.es6.eu" target="_blank">Közoktatási oldal</a>
 			<a class="btn btn-outline-secondary btn-sm" href="https://tomuwhu.github.io/sva/" target="_blank">Ságváris oktatás</a>
 			<a class="btn btn-outline-primary btn-sm" href="gimn_progr" target="_blank">Programozás szakkör</a>
-			<a class="btn btn-outline-success btn-sm" href="https://gyakg.es6.eu" target="_blank">Közoktatási oldal</a>
-			<a class="btn btn-outline-secondary btn-sm" href="https://www.inf.u-szeged.hu/~tnemeth" target="_blank">Hivatalos egyetemi oldal</a>
-			<a class="btn btn-outline-secondary btn-sm y" href="https://tomuwhu.github.io/a2gyak24/" target="_blank"
-				>Alga II gyak.</a
-			>
+			<a class="btn btn-outline-secondary btn-sm y" href="https://tomuwhu.github.io/a2gyak24/" target="_blank">Alga II gyak.</a>
 </div>
 
 <div class="container text-center">
