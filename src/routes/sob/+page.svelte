041a928fcf09ec315da55d4b564ce5db48d62ef4
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-09-06 08:58:33 +0200

    .

--- src/routes/sob/+page.svelte
+++ src/routes/sob/+page.svelte
@@ -1,4 +1,18 @@
 <h1>Szakképzési oldal 2024/25</h1>
+<table>
+    <tr>
+        <th>Óra sorszáma</th>
+        <th>Dátum</th>
+        <th>Forrás</th>
+        <th>Online kipróbálható</th>
+    </tr>
+    <tr>
+        <td>1-3</td>
+        <td>2024.09.05.</td>
+        <a href="https://github.com/tomuwhu/2024szft_example01">HTML+CSS+JS</a>
+        <td><a href="https://tomuwhu.github.io/2024szft_example01/">Online</a></td>
+    </tr>
+</table>
 <style>
     :global(body) {
         background-color: #d6dba9;
