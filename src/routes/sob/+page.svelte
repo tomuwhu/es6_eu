0794ef6a5b78b86f842b173e6fe49dd10442a280
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-09-12 13:02:16 +0200

    .

--- src/routes/sob/+page.svelte
+++ src/routes/sob/+page.svelte
@@ -1,83 +1,118 @@
 <h1>Szakképzési oldal 2024/25</h1>
-<hr>
-<a class="x" href="https://discord.gg/q4G2k479Zw" target="_blank">Discord Server Invite: q4G2k479Zw</a>
-<hr>
+<hr />
+<a class="x" href="https://discord.gg/q4G2k479Zw" target="_blank"
+	>Discord Server Invite: q4G2k479Zw</a
+>
+<hr />
 <table id="t1">
-    <tr>
-        <th class="id">Óra sorszáma</th>
-        <th class="date">Dátum</th>
-        <th>Téma</th>
-        <th>Forrás</th>
-        <th>Online kipróbálható</th>
-    </tr>
-    <tr>
-        <td class="id">1-2.</td>
-        <td class="date">2024.09.05.</td>
-        <td class="t">HTML+CSS alap</td>
-        <td class="i1"><a target="_blank" href="https://github.com/tomuwhu/2024szft_example01">GitHub</a></td>
-        <td class="i2"><a target="_blank" href="https://tomuwhu.github.io/2024szft_example01/">Kipróbál</a></td>
-    </tr>
-    <tr>
-        <td class="id">3.</td>
-        <td class="date">2024.09.05.</td>
-        <td class="t">JavaScript</td>
-        <td class="i1"><a target="_blank" href="https://github.com/tomuwhu/2024szft_example01/blob/main/p2/engine.js">GitHub</a></td>
-        <td class="i2"><a target="_blank" href="https://tomuwhu.github.io/2024szft_example01/p2">Kipróbál</a></td>
-    </tr>
-    <tr>
-        <td class="id">4-7.</td>
-        <td class="date">2024.09.11.</td>
-        <td class="t">JavaScript</td>
-        <td class="i1"><a target="_blank" href="https://github.com/tomuwhu/2024szft_example01/blob/main/p3/egy.js">GitHub</a></td>
-        <td class="i2"><a target="_blank" href="https://tomuwhu.github.io/2024szft_example01/p3">Kipróbál</a></td>
-    </tr>
+	<tr>
+		<th class="id">Óra sorszáma</th>
+		<th class="date">Dátum</th>
+		<th>Téma</th>
+		<th>Forrás</th>
+		<th>Online kipróbálható</th>
+	</tr>
+	<tr>
+		<td class="id">1-2.</td>
+		<td class="date">2024.09.05.</td>
+		<td class="t">HTML+CSS alap</td>
+		<td class="i1"
+			><a target="_blank" href="https://github.com/tomuwhu/2024szft_example01">GitHub</a></td
+		>
+		<td class="i2"
+			><a target="_blank" href="https://tomuwhu.github.io/2024szft_example01/">Kipróbál</a></td
+		>
+	</tr>
+	<tr>
+		<td class="id">3.</td>
+		<td class="date">2024.09.05.</td>
+		<td class="t">JavaScript</td>
+		<td class="i1"
+			><a
+				target="_blank"
+				href="https://github.com/tomuwhu/2024szft_example01/blob/main/p2/engine.js">GitHub</a
+			></td
+		>
+		<td class="i2"
+			><a target="_blank" href="https://tomuwhu.github.io/2024szft_example01/p2">Kipróbál</a></td
+		>
+	</tr>
+	<tr>
+		<td class="id">4-7.</td>
+		<td class="date">2024.09.11.</td>
+		<td class="t">JavaScript</td>
+		<td class="i1"
+			><a target="_blank" href="https://github.com/tomuwhu/2024szft_example01/blob/main/p3/egy.js"
+				>GitHub</a
+			></td
+		>
+		<td class="i2"
+			><a target="_blank" href="https://tomuwhu.github.io/2024szft_example01/p3">Kipróbál</a></td
+		>
+	</tr>
+	<tr>
+		<td class="id">8-10.</td>
+		<td class="date">2024.09.12.</td>
+		<td class="t">SvelteKit Static Site</td>
+		<td class="i1"
+			><a target="_blank" href="https://github.com/tomuwhu/sov02_sveltekit">GitHub</a></td
+		>
+		<td class="i2">
+			<a target="_blank" href="https://tomuwhu.github.io/sov02_sveltekit/">Eredményjelző</a> |
+			<a target="_blank" href="https://tomuwhu.github.io/sov02_sveltekit/f01">Mátrix</a>
+		</td>
+	</tr>
 </table>
+
 <style>
-    :global(body) {
-        background-color: #d6dba9;
-        text-align: center;
-        padding: 10px;
-    }
-    h1 {
-        font-family: 'Courier New', Courier, monospace;
-        padding: 20px;
-    }
-    table#t1 {
-        margin: auto;
-        border-collapse: separate;
-        border-spacing: 20px 5px;
-    }
-    .id, .date {
-        text-align: right;
-    }
-    td.i1, td.i2 {
-        width: 200px;
-        border-radius: 5px;
-        box-shadow: 1px 1px 3px black;
-        background-color: #a9dbd4;
-    }
-    td.t {
-        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-        text-shadow: 1px 1px 4px gray;
-        color: rgb(37, 78, 95);
-    }
-    td.i1:hover, td.i2:hover {
-        background-color: #80b1ab;
-    }
-    a {
-        text-decoration: none;
-    }
-    a.x {
-        padding: 10px;
-        margin:10px;
-        width: 200px;
-        border-radius: 10px;
-        box-shadow: 1px 1px 3px black;
-        background-color: #a9cedb;
-    }
-    a.x:hover {
-        background-color: #a9dbd4;
-        box-shadow: 1px 1px 5px black;
-        border: solid 3px rgb(159, 69, 69);
-    }
+	:global(body) {
+		background-color: #d6dba9;
+		text-align: center;
+		padding: 10px;
+	}
+	h1 {
+		font-family: 'Courier New', Courier, monospace;
+		padding: 20px;
+	}
+	table#t1 {
+		margin: auto;
+		border-collapse: separate;
+		border-spacing: 20px 5px;
+	}
+	.id,
+	.date {
+		text-align: right;
+	}
+	td.i1,
+	td.i2 {
+		width: 200px;
+		border-radius: 5px;
+		box-shadow: 1px 1px 3px black;
+		background-color: #a9dbd4;
+	}
+	td.t {
+		font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+		text-shadow: 1px 1px 4px gray;
+		color: rgb(37, 78, 95);
+	}
+	td.i1:hover,
+	td.i2:hover {
+		background-color: #80b1ab;
+	}
+	a {
+		text-decoration: none;
+	}
+	a.x {
+		padding: 10px;
+		margin: 10px;
+		width: 200px;
+		border-radius: 10px;
+		box-shadow: 1px 1px 3px black;
+		background-color: #a9cedb;
+	}
+	a.x:hover {
+		background-color: #a9dbd4;
+		box-shadow: 1px 1px 5px black;
+		border: solid 3px rgb(159, 69, 69);
+	}
 </style>
