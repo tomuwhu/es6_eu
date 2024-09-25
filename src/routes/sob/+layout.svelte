e918aa981cc6efcf2a1cc546ea59e9eb38f65a1e
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-09-25 07:55:18 +0200

    .

--- src/routes/sob/+layout.svelte
+++ src/routes/sob/+layout.svelte
@@ -1,6 +1,33 @@
+<script>
+	import { page } from '$app/stores'
+	$: currentRoute = $page.url.pathname
+	const menu = [
+		{ name: 'Órai munka és tananyagok', href: '/sob' },
+		{ name: 'Portfólió követelmények', href: '/sob/portfolio' },
+		{ name: 'Frontend tematika', href: '/sob/frontend' }
+	]
+</script>
+
 <div>
-	<a href="/sob/">Órai munka és tananyagok</a> -
-	<a href="/sob/portfolio">Portfólió követelmények</a> -
-	<a href="/sob/frontend">Frontend tematika</a>
+	{#each menu as item}
+		<a class={currentRoute === item.href ? 'selected' : 'norm'} href={item.href}>{item.name}</a>
+	{/each}
 </div>
 <slot />
+
+<style>
+	a {
+		padding: 5px;
+		margin: 3px;
+		border-radius: 7px;
+		background-color: rgb(186, 217, 207);
+		box-shadow: 1px 1px 4px black;
+	}
+	a.selected {
+		background-color: black;
+		color: white;
+	}
+	a:hover {
+		background-color: rgb(85, 107, 99);
+	}
+</style>
